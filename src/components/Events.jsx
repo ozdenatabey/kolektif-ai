003132f77af6074a738c816f794f57cf2f3b766c
ozdenatabey <ozdenatabey44@gmail.com>
2024-10-02 17:53:50 +0300

    added formik, need to save event name FIX IT

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,9 +1,22 @@
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { events } from "../data/eventData";
 import { gridCalc } from "../data/GridCalc";
+// import RegisterForm from "./RegisterForm";
+import { Formik, Form, ErrorMessage, Field } from "formik";
 
 function Events() {
-  console.log(gridCalc.colCountLg);
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    age: "",
+    course: "",
+  };
+  const onSubmit = (values) => {
+    // alert(JSON.stringify(values, null, 2));
+    console.log(values);
+  };
 
   return (
     <div>
@@ -45,13 +58,64 @@ function Events() {
                   </div>
                 </div>
                 <div className="flex card-actions justify-center">
-                  <button className="btn btn-neutral btn-wide items-center">
+                  <button
+                    onClick={() =>
+                      document.getElementById(event.id).showModal()
+                    }
+                    className="btn btn-neutral btn-wide items-center"
+                  >
                     Kayıt
                     <PencilSquareIcon className="size-6" />
                   </button>
                 </div>
               </div>
             </div>
+            <dialog id={event.id} className="modal m-auto">
+              <div className="modal-box">
+                <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                  {() => (
+                    <Form>
+                      <p
+                        className="font-bold text-lg mb-5"
+                        name="course"
+                        value={event.name}
+                        id={event.id}
+                      >
+                        {event.name}
+                      </p>
+                      <label htmlFor="firstName">Ad</label>
+                      <Field
+                        name="firstName"
+                        className="input input-bordered w-full mb-2"
+                      />
+                      <label>Soyad</label>
+                      <Field
+                        name="lastName"
+                        className="input input-bordered w-full mb-2"
+                      />
+                      <label>E-mail</label>
+                      <Field
+                        name="email"
+                        className="input input-bordered w-full mb-2"
+                      />
+                      <label>Telefon</label>
+                      <Field
+                        name="phone"
+                        className="input input-bordered w-full mb-2"
+                      />
+                      <label>Yaş</label>
+                      <Field
+                        name="age"
+                        className="input input-bordered w-full mb-2"
+                      />
+                      <button className="btn btn-neutral" type="submit">
+                        Gönder
+                      </button>
+                    </Form>
+                  )}
+                </Formik>
+              </div>
+            </dialog>
           </div>
         ))}
       </div>
